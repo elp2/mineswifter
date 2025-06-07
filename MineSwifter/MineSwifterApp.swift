9b281b4687ee32e5155e2e2ad2f9f13d2a35d358
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-06-07 11:24:35 -0700

    Create project in Xcode.
    TESTED=Ran in Xcode.

---
+++ MineSwifter/MineSwifterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineSwifterApp.swift
+//  MineSwifter
+//
+//  Created by Edward Palmer on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MineSwifterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MineSwifterTests/MineSwifterTests.swift
@@ -0,0 +1,17 @@
+//
+//  MineSwifterTests.swift
+//  MineSwifterTests
+//
+//  Created by Edward Palmer on 6/7/25.
+//
+
+import Testing
+@testable import MineSwifter
+
+struct MineSwifterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
