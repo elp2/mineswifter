b006a32286e15b047df37c08693b3d643107cac7
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-06-07 12:09:12 -0700

    Add top level UI features.
    TESTED=Looks good on simulator with Safe Areas

--- MineSwifter/ContentView.swift
+++ MineSwifter/ContentView.swift
@@ -1,21 +1,50 @@
-//
-//  ContentView.swift
-//  MineSwifter
-//
-//  Created by Edward Palmer on 6/7/25.
-//
-
 import SwiftUI
 
+enum Tool: Hashable { case reveal, flag, move }
+
 struct ContentView: View {
+    @State var minesLeft: String = "10"
+    @State var seconds: String = "00:00"
+    @State var onReset: () -> Void = {}
+    @State var tool: Tool = .reveal
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Spacer() // TODO: Game baord.
         }
-        .padding()
+        .safeAreaInset(edge: .top, spacing: 0) {
+            HStack {
+                Label("\(minesLeft)", systemImage: "flag.fill")
+                    .labelStyle(.titleAndIcon)
+                    .font(.title3.monospacedDigit())
+
+                Spacer(minLength: 32)
+
+                Button(action: onReset) { Text("💥").font(.title) }
+
+                Spacer(minLength: 32)
+
+                Label("\(seconds)", systemImage: "timer")
+                    .labelStyle(.titleAndIcon)
+                    .font(.title3.monospacedDigit())
+            }
+            .padding([.horizontal, .top])
+            .background {
+                Rectangle()
+                    .fill(.thinMaterial)
+                    .ignoresSafeArea(edges: .top)
+            }
+        }
+        .safeAreaInset(edge: .bottom, spacing: 0) {
+            Picker("Tool", selection: $tool) {
+                Text("Reveal").tag(Tool.reveal)
+                Text("Flag").tag(Tool.flag)
+                Text("Move").tag(Tool.move)
+            }
+            .pickerStyle(.segmented)
+            .padding([.horizontal, .top])
+            .background(.thinMaterial)
+        }
     }
 }
 
